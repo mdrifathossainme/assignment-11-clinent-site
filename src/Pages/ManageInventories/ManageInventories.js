2470fd84a463cea0acee576a6d7950a01ce955d1
Md Rifat Hossain <mdrifathossain.mr@gmail.com>
2022-05-08 05:55:51 +0600

    pagination done

--- src/Pages/ManageInventories/ManageInventories.js
+++ src/Pages/ManageInventories/ManageInventories.js
@@ -2,49 +2,82 @@ import React, { useEffect, useState } from 'react';
 import AllSingleItem from './AllSingleItem/AllSingleItem';
 import './ManageInventories.css'
 const ManageInventories = () => {
-    const [products, setProducts]=useState([])
+    const [ItemCount,setItemCount]=useState(0)
+    const [page,setPage]=useState(0);
+    const [size,setSize]=useState(5)
+    const [item, setItem] = useState([]);
+
+
+
+    useEffect(()=>{
+        fetch('https://fierce-citadel-13028.herokuapp.com/itemCount')
+        .then(res=>res.json())
+        .then(data=>{
+            const count= data.count;
+            const page= Math.ceil(count/5)
+            setItemCount(page)
+        })
+    },[])
+
+
+
     useEffect(()=>{
 
         const getItem=async()=>{
-         const url=`https://fierce-citadel-13028.herokuapp.com/additem/`
+         const url=`https://fierce-citadel-13028.herokuapp.com/alladditem/?page=${page}&size=${size}`
  
          fetch(url)
          .then(res=>res.json())
-         .then(data=> setProducts(data))
+         .then(data=> setItem(data))
         }
         getItem()
-     },[])
+     },[item,size])
      const handelManegDeleteItem=( id)=>{
         const sure= window.confirm('Are you Sure')
         if(sure){
-          const url=`https://fierce-citadel-13028.herokuapp.com/additem/${id}`
+          const url=`https://fierce-citadel-13028.herokuapp.com/alladditem/${id}`
           fetch(url,{
           method:"delete"
       })
       .then(rea=>rea.json())
       .then(data=>{
           if(data.deletedCount>0){
-              const remaining= products.filter(pt=>pt._id!==id)
-              setProducts(remaining)
+              const remaining= item.filter(pt=>pt._id!==id)
+              setItem(remaining)
           }
       })
       } 
         }
-console.log(products)
+console.log(setItem)
     return (
-
-        <div className='mannageItem'>
+<>
+<div className='mannageItem'>
            <div className='container' >
           <div className="mannageIte-collection">
           <h1>My All Item</h1>  
           <div className='mannageIte-itemSection'> 
           {
-              products.map(item=><AllSingleItem key={item._id} item={item} handelManegDeleteItem={handelManegDeleteItem}></AllSingleItem>)
+              item.map(item=><AllSingleItem key={item._id} item={item} handelManegDeleteItem={handelManegDeleteItem}></AllSingleItem>)
           }
         </div>
           </div>
         </div>
         </div>
+            <div className='pagenation'>
+                {
+                    [...Array(ItemCount).keys()].map(number=><button
+                  className={page===number? "selected":""}
+                    onClick={()=>setPage(number)}
+                    >{number+1}</button>)
+                }
+            <select onChange={e=>setSize(e.target.value)}>
+                <option value="5" selected>5</option>
+                <option value="10">10</option>
+                <option value="15">15</option>
+            </select>
+            </div>
+</>
+       
     );
 };
 
